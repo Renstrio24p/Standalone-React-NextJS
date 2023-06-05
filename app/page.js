50e80ca562acd7146d1dfef8068d54975cefdf32
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-06-05 13:00:28 +0800

    Standalone React - NextJS

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import styles from './styles/modules/page.module.css'
+import Start from './pages/start'
+
+export default function Home() {
+  return (
+    <main>
+      <Start />
+    </main>
+  )
+}
---
+++ app/pages/start.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactApp from "./react-18/react";
+
+export default function Start() {
+    return (
+        <div>
+            <ReactApp />
+            {/* You can start coding right here..*/}
+        </div>
+    )
+}
+
+// This is the main point of your project
